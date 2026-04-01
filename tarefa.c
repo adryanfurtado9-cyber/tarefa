a9eb7f7f8f7b9d9a19f3517dd2ba0b8204b37321
Adryan <adryanfurtado9@gmail.com>
2026-04-01 20:25:00 -0300

    Add age check program in tarefa.c

---
+++ tarefa.c
@@ -0,0 +1,47 @@
+#include <stdio.h> 
+
+  
+
+int main() { 
+
+  
+
+int idade; 
+
+printf("tu e velho? "); 
+
+scanf("%d", &idade); 
+
+  
+
+if (idade >= 18 && idade <= 9999) { 
+
+printf("tem que pagar boleto :(\n"); 
+
+} else { 
+
+printf("nao pode usar chat do roblox agradeçe o felca :(\n"); 
+
+} 
+
+ 
+
+if(idade % 2 == 0){ 
+
+    printf("a idade é par"); 
+
+ } 
+
+  
+
+ else { 
+
+     printf ("a idade é ímpar"); 
+
+ } 
+
+  
+
+  
+
+} 
